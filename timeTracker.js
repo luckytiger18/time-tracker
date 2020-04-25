9dc47592864344ca9174d9da495e98e2d867e4de
Annie Luc <lannie3186@gmail.com>
2020-04-25 09:42:24 -0700

    printing the timestamp to the page

--- timeTracker.js
+++ timeTracker.js
@@ -1,4 +1,7 @@
-var dateNow = Date(Date.now())
-function startButton () {
-    alert(dateNow)
+function startButton() {
+    var dateNow = Date(Date.now());
+    var getList = document.getElementById("list");
+    var listItem = document.createElement("P")
+    listItem.innerText = dateNow
+    getList.appendChild(listItem)
 }
