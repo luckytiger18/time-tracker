83d754c80e152ce317f6bcad054bfeb6ee3565d2
Annie Luc <lannie3186@gmail.com>
2020-04-25 10:04:14 -0700

    changed the date format

--- timeTracker.js
+++ timeTracker.js
@@ -1,7 +1,13 @@
 function startButton() {
-    var dateNow = Date(Date.now());
+    var dateNow = new Date();
     var getList = document.getElementById("list");
-    var listItem = document.createElement("P")
-    listItem.innerText = dateNow
-    getList.appendChild(listItem)
+    var listItem = document.createElement("P");
+    var strDate = "Start: " + dateNow.getMonth() + "/" + dateNow.getDate() + "/" + dateNow.getFullYear()
+    listItem.innerText = strDate
+    getList.appendChild(listItem);
+}
+
+function stopButton() {
+
+    document.getElementById("taskInput")
 }
