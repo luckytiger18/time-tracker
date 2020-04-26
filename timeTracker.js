3a2f4735280b7808e4cba185f37ecc92c204be59
Annie Luc <lannie3186@gmail.com>
2020-04-25 22:13:20 -0700

    added function for stopTask

--- timeTracker.js
+++ timeTracker.js
@@ -2,12 +2,16 @@ function startButton() {
     var dateNow = new Date();
     var getList = document.getElementById("list");
     var listItem = document.createElement("P");
-    var strDate = "Start: " + dateNow.getMonth() + "/" + dateNow.getDate() + "/" + dateNow.getFullYear()
+    var strDate = "Start: "+ dateNow.getMonth() + "/" + dateNow.getDate() + "/" + dateNow.getFullYear()
     listItem.innerText = strDate
     getList.appendChild(listItem);
 }
 
-function stopButton() {
-
-    document.getElementById("taskInput")
+function stopTask() {
+   var textValue = document.getElementById("taskInput").value;
+   var getList = document.getElementById("list");
+   var newTask = document.createElement("P");
+   newTask.innerText = "Task: " + textValue
+   getList.appendChild(newTask)
+  document.getElementById("taskInput").innerText = "";
 }
